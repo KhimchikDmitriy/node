03125bc6935bc9fd544e43d29f2348170a7682f4
= <=>
2023-10-17 14:07:47 +0300

    1

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-1;
+const path = require("path");
+const fs = require("fs");
+// fs.mkdir(path.join(__dirname, "tmp"), function (err) {
+//   if (err) throw err;
+//   console.log("папка создана");
+// });
+const filePath = path.join(__dirname, "tmp", "2.txt");
+console.log(filePath);
+// fs.writeFile(filePath, "Something wrong your file path", function (err) {
+//   if (err) throw err;
+//   console.log("файл создан");
+// });
+// fs.appendFile(filePath, "~ara ", function (err) {
+//   if (err) throw err;
+//   console.log("файл изменен");
+// });
+fs.readFile(filePath, "UTF-8", (err, data) => {
+  if (err) {
+    console.error(err);
+  }
+  console.log(data);
+});
