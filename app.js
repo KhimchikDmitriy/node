03541343f3c161e6d9531b389fdc927b76502ed6
= <=>
2023-10-17 15:00:26 +0300

    project_start

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
-const path = require("path");
-const fs = require("fs");
-// fs.mkdir(path.join(__dirname, "tmp"), function (err) {
-//   if (err) throw err;
-//   console.log("папка создана");
-// });
-const filePath = path.join(__dirname, "tmp", "2.txt");
-console.log(filePath);
-// fs.writeFile(filePath, "Something wrong your file path", function (err) {
-//   if (err) throw err;
-//   console.log("файл создан");
-// });
-// fs.appendFile(filePath, "~ara ", function (err) {
-//   if (err) throw err;
-//   console.log("файл изменен");
-// });
-fs.readFile(filePath, "UTF-8", (err, data) => {
-  if (err) {
-    console.error(err);
-  }
-  console.log(data);
+import express from "express";
+const app = express();
+const port = "3000";
+app.listen(port, () => {
+  console.log("listening port: " + port);
+});
+app.get("/", function (req, res) {
+  res.send("<h1 style = 'color: green'>Hello kukuembers!</h1>");
+  console.log();
 });
