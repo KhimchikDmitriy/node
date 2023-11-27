7748b28a820ade569640a8898ca7ff4ca08f97f7
= <=>
2023-11-27 13:42:11 +0300

    commits

--- app.js
+++ app.js
@@ -6,15 +6,15 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import Sequelize from "sequelize";
 import sqlite3 from "sqlite3";
-// const db = new sqlite3.Database(":memory:");
-// import { sequelize } from './db';
+const db = new sqlite3.Database(":memory:");
+// import { sequelize } from "./test.sqlite"; // may be 22
 import fs from "fs";
 import ejs from "ejs";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = "3000";
 const currentTime = new Date().toLocaleString();
-import myRoutes from "./routers/index_routers.js";
+// import myRoutes from "./routers/index_routers.js"; // and 46
 
 app.set("view engine", "ejs");
 app.set("\views", __dirname + "views");
@@ -22,6 +22,9 @@ app.set("\views", __dirname + "views");
 const sequelize = new Sequelize({
   dialect: "sqlite",
   storage: "test.sqlite",
+  define: {
+    timestamps: false,
+  },
 });
 
 app.use(express.json());
@@ -40,7 +43,7 @@ app.use(
   )
 );
 app.use(favicon(join(__dirname, "/public/img/ico.png")));
-app.use(myRoutes);
+// app.use(myRoutes); //commented 60-90
 
 app.listen(port, () => {
   console.log("...");
@@ -54,37 +57,55 @@ app.listen(port, () => {
   console.log("в данный момент используется версия " + app.get("env"));
 });
 
-// app.get("/as", (req, res) => {
-//   res.sendFile(__dirname + "/public/index.html");
-//   addline("logging completes /as");
-// });
-// app.get("/", (req, res) => {
-//   res.end("dea ex machina");
-//   addline("logging completes /");
-// });
-// app.get("/test", (req, res) => {
-//   res.end("deus ex machina");
-//   addline("logging completes /test");
-// });
-// app.post("/as", (req, res) => {
-//   console.log("...");
-//   console.log("проверка post пройдена");
-//   console.log("...");
-//   console.log(req.body);
-//   console.log("password: " + req.body.pass);
-//   console.log("name: " + req.body.name);
-//   res.end("проверка post пройдена.");
-// });
-// app.post("/test", (req, res) => {
-//   console.log("...");
-//   console.log("проверка post пройдена");
-//   console.log("...");
-//   console.log(req.body);
-//   console.log(req.url);
-//   console.log("password: " + req.body.pass);
-//   console.log("name: " + req.body.name);
-//   res.end("проверка post пройдена.");
-// });
+app.get("/as", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+  addline("logging completes /as");
+});
+app.get("/", (req, res) => {
+  res.end("dea ex machina");
+  addline("logging completes /");
+});
+app.get("/test", (req, res) => {
+  res.end("deus ex machina");
+  addline("logging completes /test");
+});
+app.post("/as", (req, res) => {
+  console.log("...");
+  console.log("проверка post пройдена");
+  console.log("...");
+  console.log(req.body);
+  console.log("password: " + req.body.pass);
+  console.log("name: " + req.body.name);
+  res.end("проверка post пройдена.");
+});
+app.post("/test", (req, res) => {
+  console.log("...");
+  console.log("проверка post пройдена");
+  console.log("...");
+  console.log(req.body);
+  console.log(req.url);
+  console.log("password: " + req.body.pass);
+  console.log("name: " + req.body.name);
+  res.end("проверка post пройдена.");
+});
+
+const User = sequelize.define("user", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  age: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+});
+
 function addline(line) {
   line = line + " timestamp: " + currentTime + "\n";
   fs.appendFile(__dirname + "/logger/logger.txt", line, (err) => {
